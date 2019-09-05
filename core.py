9074431be0f325e2d016e13e8ea8e9da65e2ead2
Joscha Götzer <joscha.goetzer@gmail.com>
2019-09-05 23:45:32 +0200

    Implemented taco transfers by @mention. Required: - username column on db table - some minor bug fixes - breaking out a send_tacos() method - storing all users in a handler with group=-1

--- core.py
+++ core.py
@@ -3,7 +3,7 @@ import logging
 from decouple import config
 import os
 
-from handlers.basic import help_handler, start_handler
+from handlers.basic import help_handler, start_handler, store_names_handler
 from handlers.leaderboards import taco_top_handler, my_tacos_handler
 from handlers.setup import init_taco_handler, self_kick_handler, new_chat_handler
 from handlers.tacotransfers import chat_reply_handler, taco_mention_handler
@@ -23,6 +23,9 @@ upd = Updater(bot_token,                                                        
 dp = upd.dispatcher
 
 if __name__ == '__main__':                                                               # adding handlers to dispatcher
+    # Always execute first, synchronously
+    dp.add_handler(store_names_handler, group=-1)
+
     dp.add_handler(start_handler)
     dp.add_handler(help_handler)
     dp.add_handler(new_chat_handler)
